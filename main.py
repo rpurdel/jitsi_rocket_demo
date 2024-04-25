25231dd3caff6213ed88c8d1dcf4d15f08743a1c
RazvanP <razvan.purdel@8x8.com>
2024-04-25 14:23:24 +0300

    Add index

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ def meet(meeting_id):
                            tenant=session['tenant']
                            )
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
